a75b9c80e04e1109518a56244b4fe0f5c58f36d1
Diva Krishna <divakrishnam@gmail.com>
2021-12-01 21:03:41 +0800

    initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+const path = require('path');
+console.log('.env.' + process.env.NODE_ENV);
+dotenv.config({
+  path: path.resolve(__dirname, '.env.' + process.env.NODE_ENV),
+});
+module.exports = {
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  HOST: process.env.HOST || '127.0.0.1',
+  PORT: process.env.PORT || 3000,
+};
